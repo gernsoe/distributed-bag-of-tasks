3f09f971fb42083484dc72e25a48b216cb772c5e
Christian <christian.gernsoe@gmail.com>
2021-02-18 15:11:36 +0100

    Made bag a blocking queue and implemented workers as threads

--- Bag.java
+++ Bag.java
@@ -1,25 +1,59 @@
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 class Bag {
-    private Queue<Task> taskBag;
+
+    private ArrayList<Worker> workers;
+    private BlockingQueue<Task> taskBag;
     public static List<Future<Integer>> futures;
 
-    public Bag(){
-        this.taskBag = new LinkedList<Task>(){};
+
+    public Bag(int numberOfWorkers){
+        this.taskBag = new LinkedBlockingQueue<>(){};
         this.futures = new ArrayList<Future<Integer>>() {};
+        this.workers = new ArrayList<>(){};
+        for (int i = 0; i < numberOfWorkers; ++i) {
+            workers.add(new Worker(this));
+        }
     }
 
     public void addTask(Task task) {
-        taskBag.add(task);
+        try {
+            taskBag.put(task);
+        } catch (InterruptedException e) {}
+
     }
 
-    public Task getTask() { return taskBag.poll(); }
+    public Task getTask() {
+        Task task = null;
+        try {
+            task = taskBag.take();
+        } catch (InterruptedException e) {}
+
+        return task;
+    }
 
     public List<Future<Integer>> getFutures() { return futures; }
 
     public void addFuture(Future f) { futures.add(f); }
 
+}
+
+class Worker extends Thread {
+    Bag bag;
+
+    public Worker(Bag bag) {
+        this.bag = bag;
+    }
+
+    public void run() {
+        while (true) {
+            Task task = bag.getTask();
+
+            System.out.println("Started working on task with input: " + task.getID());
+
+            task.run();
+        }
+    }
 }
 
