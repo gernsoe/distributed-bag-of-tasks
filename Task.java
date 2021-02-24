980b237f926267054f07554903f7f6240ad551c9
MarcBrejner <marc@kaabrejner.dk>
2021-02-24 13:48:45 +0100

    Made task generic

--- Task.java
+++ Task.java
@@ -1,15 +1,23 @@
 import java.util.concurrent.Callable;
 
-abstract class Task implements Callable<Integer>, Runnable {
+abstract class Task<T> implements Callable<T>, Runnable {
     int ID;
     Boolean isDone = false;
-    Integer result;
+    T result;
 
     public Task(int ID) {
         this.ID = ID;
     }
 
-    public synchronized Integer getResult() {
+    public void run(){
+        T r;
+        try{
+          r = call();
+        } catch(Exception e){return;}
+        setResult(r);
+    }
+
+    public synchronized T getResult() {
         while(!isDone){
             try{
                 wait();
@@ -19,7 +27,7 @@ abstract class Task implements Callable<Integer>, Runnable {
         return result;
     }
 
-    public synchronized void setResult(Integer result) {
+    public synchronized void setResult(T result) {
         this.result = result;
         isDone = true;
         notifyAll();
--- primeTask.java
+++ primeTask.java
@@ -11,10 +11,7 @@ public class primeTask extends Task {
         return (int) Math.floor((fact(numberToFind)%(numberToFind+1))/numberToFind)*(numberToFind-1)+2;
     }
 
-    public void run(){
-        int r = call();
-        setResult(r);
-    }
+
 
     public int fact(int n) {
         if (n==0) {
